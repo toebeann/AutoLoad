304bd6401962751175e08ae71db9f6c43cf6f7b2
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-27 17:25:54 +0000

    Added Config file and Options templates.

---
+++ AutoLoad/Options.cs
@@ -0,0 +1,29 @@
+﻿using SMLHelper.V2.Options;
+
+namespace Straitjacket.Subnautica.Mods.AutoLoad
+{
+    internal class Options : ModOptions
+    {
+        public override void BuildModOptions()
+        {
+            AddKeybindOption("overrideKey", "Skip AutoLoad on launch",
+                GameInput.GetPrimaryDevice(), AutoLoad.Config.OverrideKey);
+        }
+
+        public Options() : base("AutoLoad")
+        {
+            KeybindChanged += Options_KeybindChanged;
+        }
+
+        private void Options_KeybindChanged(object sender, KeybindChangedEventArgs e)
+        {
+            switch (e.Id)
+            {
+                case "overrideKey":
+                    AutoLoad.Config.OverrideKey = e.Key;
+                    break;
+            }
+            AutoLoad.Config.Save();
+        }
+    }
+}
