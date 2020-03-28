32a1236eee79c09f9394e710f3b67b51e7c084ca
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-28 22:23:14 +0000

    Added config options for AutoLoad mode (Most recently loaded vs Most recently saved [default]), mode override key, and specific save slot.

--- AutoLoad/Config.cs
+++ AutoLoad/Config.cs
@@ -1,10 +1,15 @@
-﻿using Straitjacket.Subnautica.Mods.AutoLoad.SMLHelper.V2.Options;
+﻿using Oculus.Newtonsoft.Json;
+using SMLHelper.V2.Json;
 using UnityEngine;
 
 namespace Straitjacket.Subnautica.Mods.AutoLoad
 {
-    internal class Config : ModConfig
+    internal class Config : ConfigFile
     {
         public KeyCode OverrideKey { get; set; } = KeyCode.Escape;
+        public AutoLoadMode AutoLoadMode { get; set; } = AutoLoadMode.MostRecentlySaved;
+        public KeyCode ToggleAutoLoadModeKey { get; set; } = KeyCode.LeftControl;
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string SpecificSaveSlot { get; set; } = null;
     }
 }
