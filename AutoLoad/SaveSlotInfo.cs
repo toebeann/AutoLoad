e6a54e89861e10de891b0e4211d242b37ae8bd67
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-04-14 02:01:15 +0100

    SaveSlotInfo object definition.

---
+++ AutoLoad/SaveSlotInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Straitjacket.Subnautica.Mods.AutoLoad
+{
+    internal class SaveSlotInfo
+    {
+        public string SaveGame { get; set; }
+        public GameMode GameMode { get; set; }
+        public string Session { get; set; }
+    }
+}
