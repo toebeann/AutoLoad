f660b623636655a308e3b9ea5658e7c6d65da117
Tobey Blaber <tobey@straitjacket.software>
2020-09-16 04:08:07 +0100

    Updated to SMLHelper 2.9 and converted to new options menu

--- AutoLoad/Config.cs
+++ AutoLoad/Config.cs
@@ -1,17 +1,29 @@
 ﻿using Oculus.Newtonsoft.Json;
 using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
 using UnityEngine;
 
 namespace Straitjacket.Subnautica.Mods.AutoLoad
 {
+    [Menu("AutoLoad", LoadOn = MenuAttribute.LoadEvents.MenuRegistered | MenuAttribute.LoadEvents.MenuOpened)]
     internal class Config : ConfigFile
     {
+        [Choice("Pause after AutoLoad", "Off", "AutoLoaded saves only", "All saves")]
         public AutoLoadPause PauseOnLoad { get; set; } = AutoLoadPause.Off;
-        public KeyCode OverrideKey { get; set; } = KeyCode.Escape;
+
+        [Choice("AutoLoad mode", "Most recently saved", "Most recently loaded")]
         public AutoLoadMode AutoLoadMode { get; set; } = AutoLoadMode.MostRecentlySaved;
+
+        [Keybind("Skip AutoLoad on launch")]
+        public KeyCode OverrideKey { get; set; } = KeyCode.Escape;
+
+        [Keybind("Toggle most recent load mode")]
         public KeyCode ToggleAutoLoadModeKey { get; set; } = KeyCode.LeftControl;
+
+        [Toggle("Start new game with most recent game mode")]
+        public bool StartNewGame { get; set; } = true;
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string SpecificSaveSlot { get; set; } = null;
-        public bool StartNewGame { get; set; } = true;
     }
 }
